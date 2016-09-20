c6154860e48b4340b1570d48284f624e64ead682
nemanja-m <nemanja_milicevic@hotmail.com>
2016-09-20 14:37:31 +0200

    Fix indentation

--- test/thrift_serializer_test.exs
+++ test/thrift_serializer_test.exs
@@ -5,9 +5,9 @@ defmodule ThriftSerializerTest do
   test "serialize and deserialize" do
     user = Structs.User.new(name: "Wade Winston Wilson", age: 25)
 
-    serialized = serialize(user, model: :User)
+    serialized = serialize(user, :User)
 
-    {:ok, deserialized} = deserialize(serialized, model: :User)
+    {:ok, deserialized} = deserialize(serialized, :User)
 
     assert user == %ThriftSerializerTest.Structs.User{age: 25,
         name: "Wade Winston Wilson"}
