a22d75aaead386e710178948b88eee4a5bad8115
nemanja-m <nemanja_milicevic@hotmail.com>
2016-09-21 15:44:35 +0200

    Extract binary_protocol method

--- lib/thrift_serializer/encoder.ex
+++ lib/thrift_serializer/encoder.ex
@@ -4,25 +4,25 @@ defmodule ThriftSerializer.Encoder do
     struct_definition = {:struct, {thrift_module, model}}
 
     try do
-      with {:ok, tf} <- :thrift_memory_buffer.new_transport_factory(),
-           {:ok, pf} <- :thrift_binary_protocol.new_protocol_factory(tf, []),
-           {:ok, binary_protocol} <- pf.()
-        do
-          {proto, :ok} =
-            apply(module, :to_erlang, [struct, struct_definition])
-            |> write_proto(binary_protocol, struct_definition)
+      erlang_struct = apply(module, :to_erlang, [struct, struct_definition])
 
-          {_, data} = :thrift_protocol.flush_transport(proto)
-          :erlang.iolist_to_binary(data)
-        end
+      {proto, :ok} =
+        :thrift_protocol.write(binary_protocol, {struct_definition, erlang_struct})
+
+      {_, data} = :thrift_protocol.flush_transport(proto)
+      :erlang.iolist_to_binary(data)
 
     rescue ArgumentError ->
       raise ThriftSerializer.Error, message: "Invalid field type"
     end
   end
 
-  defp write_proto(erlang_struct, protocol, struct_definition) do
-    :thrift_protocol.write(protocol, {struct_definition, erlang_struct})
+  defp binary_protocol do
+    {:ok, tf} = :thrift_memory_buffer.new_transport_factory()
+    {:ok, pf} = :thrift_binary_protocol.new_protocol_factory(tf, [])
+    {:ok, binary_protocol} = pf.()
+
+    binary_protocol
   end
 
 end
