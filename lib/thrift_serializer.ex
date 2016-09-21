7049316a88b9b4d805dcb37e67b492baeeddaee4
nemanja-m <nemanja_milicevic@hotmail.com>
2016-09-21 15:19:51 +0200

    Use __MODULE__ as parameter

--- lib/thrift_serializer.ex
+++ lib/thrift_serializer.ex
@@ -1,5 +1,10 @@
 defmodule ThriftSerializer do
-  defmacro __using__(options) do
+
+  defmodule Error do
+    defexception message: ""
+  end
+
+  defmacro __using__(_options) do
     quote do
       def thrift_module do
         [{module, _}] = @thrift_options
@@ -20,34 +25,30 @@ defmodule ThriftSerializer do
 
         ThriftSerializer.Encoder.encode(struct,
                                         get_struct_name(model, __MODULE__),
-                                        thrift_module)
+                                        thrift_module,
+                                        __MODULE__)
       end
 
       def decode(binary, [model: model]) do
         ThriftSerializer.Decoder.decode(binary,
                                         get_struct_name(model, __MODULE__),
-                                        thrift_module)
+                                        thrift_module,
+                                        __MODULE__)
       end
-    end
 
-    defp get_struct_name(model, module) do
-      Atom.to_string(model)
+      defp get_struct_name(model, module) do
+        Atom.to_string(model)
         |> String.replace("#{__MODULE__}.", "")
         |> String.to_atom
-    end
-
-    defp validate!(struct) do
-      case (Map.values(struct) |> Enum.any?(&(&1 == :undefined))) do
-        true  -> raise Error, message: "Required field is missing"
-        false -> struct
       end
+
+      defp validate!(struct) do
+        case (Map.values(struct) |> Enum.any?(&(&1 == :undefined))) do
+          true  -> raise Error, message: "Required field is missing"
+          false -> struct
+        end
+      end
+
     end
   end
 end
-
-# example
-
-# defmodule Models do
-#   use Riffed.Struct, example_types: [:User]
-#   use ThriftSerializer
-# end
--- lib/thrift_serializer/encoder.ex
+++ lib/thrift_serializer/encoder.ex
@@ -1,6 +1,6 @@
 defmodule ThriftSerializer.Encoder do
 
-  def encode(struct, model, thrift_module) do
+  def encode(struct, model, thrift_module, module) do
     struct_definition = {:struct, {thrift_module, model}}
 
     try do
@@ -9,14 +9,16 @@ defmodule ThriftSerializer.Encoder do
            {:ok, binary_protocol} <- pf.()
         do
           {proto, :ok} =
-            to_erlang(struct, struct_definition)
+            apply(module, :to_erlang, [struct, struct_definition])
             |> write_proto(binary_protocol, struct_definition)
 
           {_, data} = :thrift_protocol.flush_transport(proto)
           :erlang.iolist_to_binary(data)
         end
 
-    rescue ArgumentError -> raise Error, message: "Invalid field type" end
+    rescue ArgumentError ->
+      raise ThriftSerializer.Error, message: "Invalid field type"
+    end
   end
 
   defp write_proto(erlang_struct, protocol, struct_definition) do
