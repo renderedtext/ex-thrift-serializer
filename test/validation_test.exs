7af0bf4c69f72c171afe6493648f34b719d753df
nemanja-m <nemanja_milicevic@hotmail.com>
2016-09-21 15:23:03 +0200

    Update validation test

--- test/validation_test.exs
+++ test/validation_test.exs
@@ -2,15 +2,17 @@ defmodule ValidationTest do
   use ExUnit.Case
 
   test "Validate hash fields types" do
-    user = Structs.User.new(name: "Wade", age: "25")
+    user = %{name: "Wade", age: "25"}
 
-    assert_raise(ThriftSerializer.Error, fn -> Structs.encode(user, :User) end)
+    assert_raise(ThriftSerializer.Error,
+                 fn -> Structs.encode(user, [model: Structs.User]) end)
   end
 
   test "Validate required hash fields" do
-    user = Structs.User.new(name: "Wade")
+    user = %{name: "Wade"}
 
-    assert_raise(ThriftSerializer.Error, fn -> Structs.encode(user, :User) end)
+    assert_raise(ThriftSerializer.Error,
+                 fn -> Structs.encode(user, [model: Structs.User]) end)
   end
 
 end
