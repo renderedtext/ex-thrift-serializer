213fc9b3b11fc53a4d8ee6ab6ad03d41d2e33cdb
nemanja-m <nemanja_milicevic@hotmail.com>
2016-09-21 11:07:47 +0200

    Update serialization test

--- test/thrift_serializer_test.exs
+++ test/thrift_serializer_test.exs
@@ -1,15 +1,16 @@
 defmodule ThriftSerializerTest do
   use ExUnit.Case
 
-  test "Encode hash to binary" do
-    user = Structs.User.new(name: "Wade", age: 25)
+  test "encode hash to binary" do
+    # user = Structs.User.new(name: "Wade", age: 25)
+    user = %{name: "Wade", age: 25}
     binary = Structs.encode(user, :User)
 
     assert binary ==
       <<11, 0, 1, 0, 0, 0, 4, 87, 97, 100, 101, 8, 0, 2, 0, 0, 0, 25, 0>>
   end
 
-  test "Decode binary to hash" do
+  test "decode binary to hash" do
     user = Structs.User.new(name: "Wade", age: 25)
     binary = Structs.encode(user, :User)
 
