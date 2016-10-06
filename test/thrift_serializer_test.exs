08443039bcf333d12a5172e35a688f8671823131
Predrag Rakic <prakic@renderedtext.com>
2016-10-06 12:23:25 +0200

    Refactor serializer test

--- test/thrift_serializer_test.exs
+++ test/thrift_serializer_test.exs
@@ -1,27 +1,24 @@
 defmodule ThriftSerializerTest do
   use ExUnit.Case
 
-  test "encode struct to binary" do
+  setup do
     user = Structs.User.new(name: "Wade", age: 25)
-    binary = Structs.encode(user, model: Structs.User)
+    binary = <<11, 0, 1, 0, 0, 0, 4, 87, 97, 100, 101, 8, 0, 2, 0, 0, 0, 25, 0>>
+    model = [model: Structs.User]
 
-    assert binary ==
-      <<11, 0, 1, 0, 0, 0, 4, 87, 97, 100, 101, 8, 0, 2, 0, 0, 0, 25, 0>>
+     {:ok, [user: user, binary: binary, model: model]}
   end
 
-  test "encode struct to binary - 1 argument" do
-    user = Structs.User.new(name: "Wade", age: 25)
-    binary = Structs.encode(user)
-
-    assert binary ==
-      <<11, 0, 1, 0, 0, 0, 4, 87, 97, 100, 101, 8, 0, 2, 0, 0, 0, 25, 0>>
+  test "encode struct to binary", context do
+    assert Structs.encode(context[:user], context[:model]) == context[:binary]
   end
 
-  test "decode binary to struct" do
-    user = Structs.User.new(name: "Wade", age: 25)
-    binary = Structs.encode(user, model: Structs.User)
+  test "encode struct to binary - 1 argument", context do
+    assert Structs.encode(context[:user]) == context[:binary]
+  end
 
-    assert Structs.decode(binary, model: Structs.User) == user
+  test "decode binary to struct", context do
+    assert Structs.decode(context[:binary], context[:model]) == context[:user]
   end
 
 end
